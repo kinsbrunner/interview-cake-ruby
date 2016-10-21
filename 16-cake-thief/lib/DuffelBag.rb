4903e6e8525c8c656a54a0fca28590ac381e7bf8
kinsbrunner <kinsbrunner@hotmail.com>
2016-10-21 15:35:10 -0400

    Problem 16 - Cake Thief (initial commit)

---
+++ 16-cake-thief/lib/DuffelBag.rb
@@ -0,0 +1,4 @@
+class DuffelBag
+  def max_duffel_bag_value(cake_arrays, capacity)    
+  end
+end
---
+++ 16-cake-thief/spec/duffel_bag_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+RSpec.describe DuffelBag, type: :model do
+  describe "#max_duffel_bag_value method" do 
+    let(:bag){ DuffelBag.new }
+    
+
+  end 
+end
